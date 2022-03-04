c9be3693db932c6a5dda32471b6c66fc9888af0a
RoqaiaRF <qoraia97@gmail.com>
2022-03-04 11:54:28 +0300

    fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,62 +2,80 @@ import { useState, useEffect, FunctionComponent } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 
-const NavItem: FunctionComponent<{
-   active: string
-   setActive: Function
-   name: string
-   route: string
-}> = ({ active, setActive, name, route }) => {
-   return active !== name ? (
-      <Link href={route}>
-         <a>
-            <span
-               className='mx-2 cursor-pointer hover:border-b-4 hover:text-green'
-               onClick={() => setActive(name)}>
-               {name}
-            </span>
-         </a>
-      </Link>
-   ) : null
-}
+// const NavItem: FunctionComponent<{
+//    active: string
+//    setActive: Function
+//    name: string
+//    route: string
+// }> = ({ active, setActive, name, route }) => {
+//    return active !== name ? (
+//       <Link href={route}>
+//          <a  className='prot-nav-btn'>
+//             <span
+//                className='mx-2 cursor-pointer hover:border-b-4 hover:text-green'
+//                onClick={() => setActive(name)}>
+//                {name}
+//             </span>
+//          </a>
+//       </Link>
+//    ) : null
+// }
 
 const Navbar = () => {
    const { pathname } = useRouter()
 
    const [active, setActive] = useState('')
 
-   //later
-   useEffect(() => {
-      if (pathname === '/') setActive('About')
-      else if (pathname === '/projects') setActive('Projects')
-      else if (pathname === '/resume') setActive('Resume')
-   }, [])
+
 
    return (
-      <div className='flex items-center justify-between px-5 py-3 my-3'>
-         <span className='text-xl font-bold border-b-4 md:text-2xl border-green'>
-            {active}
-         </span>
-
+      <div className='flex items-center px-5 py-3 my-3'>
          <div className='text-base font-normal md:text-xl'>
-            <NavItem
-               active={active}
-               setActive={setActive}
-               name='About'
-               route='/'
-            />
-            <NavItem
-               active={active}
-               setActive={setActive}
-               name='Resume'
-               route='/resume'
-            />
-            <NavItem
-               active={active}
-               setActive={setActive}
-               name='Projects'
-               route='/projects'
-            />
+            <Link href={'/'}>
+            <a  className='prot-nav-btn'>
+            <span className='span-btn' ></span>
+            <span className='span-btn' ></span>
+            <span className='span-btn' ></span>
+            <span className='span-btn' ></span>
+
+               <span
+                  className='mx-2 cursor-pointer hover:border-b-4 hover:text-green'
+                  onClick={() => setActive("about")}>
+                  {"about"}
+               </span>
+            </a>
+         </Link>
+
+         <Link href={'/resume'}>
+            <a  className='prot-nav-btn'>
+            <span className='span-btn' ></span>
+            <span className='span-btn' ></span>
+            <span className='span-btn' ></span>
+            <span className='span-btn' ></span>
+
+
+               <span
+                  className='mx-2 cursor-pointer hover:border-b-4 hover:text-green'
+                  onClick={() => setActive("Resume")}>
+                  {"Resume"}
+               </span>
+            </a>
+         </Link>
+         <Link href={'/projects'}>
+            <a  className='prot-nav-btn'>
+            <span className='span-btn' ></span>
+            <span className='span-btn' ></span>
+            <span className='span-btn' ></span>
+            <span className='span-btn' ></span>
+
+               <span
+                  className='mx-2 cursor-pointer hover:border-b-4 hover:text-green'
+                  onClick={() => setActive("Projects")}>
+                  {"Projects"}
+               </span>
+            </a>
+         </Link>
+            
          </div>
       </div>
    )
